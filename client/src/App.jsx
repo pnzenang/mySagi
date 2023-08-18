1d896ca883399a88b3e0eee39200b60f319cc011
Patrice Nzenang <pnzenang@gmail.com>
2023-08-18 02:04:51 -0400

    modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/App.jsx 	new file:   client/src/assets/wrappers/HomePage.js 	modified:   client/src/pages/How.jsx 	modified:   package-lock.json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import {
   Landing,
   Register,
---
+++ client/src/assets/wrappers/HomePage.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  border-radius: var(--border-radius);
+  width: 100%;
+  background: var(--background-secondary-color);
+  padding: 3rem 2rem 4rem;
+  line-height: 1.5rem;
+  .form-title {
+    margin-bottom: 1.5rem;
+  }
+  .form {
+    margin: 0;
+    border-radius: 0;
+    box-shadow: none;
+    padding: 0;
+    max-width: 100%;
+    width: 100%;
+  }
+  .form-row {
+    margin-bottom: 0;
+  }
+  .form-center {
+    display: grid;
+    row-gap: 1rem;
+  }
+  .badge {
+    background: var(--primary-500);
+    margin-top: 1.5rem;
+    margin-bottom: 0.5rem;
+  }
+  .status-title {
+    font-weight: bold;
+  }
+  .form-btn {
+    align-self: end;
+    margin-top: 1rem;
+    display: grid;
+    place-items: center;
+  }
+  @media (min-width: 992px) {
+    .form-center {
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+      column-gap: 1rem;
+    }
+  }
+  @media (min-width: 1120px) {
+    .form-center {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+  }
+`;
+
+export default Wrapper;
